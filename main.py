77c4cfcc5fa62e87ef280f64926ad51675a2b50d
Dmitry <babanov91@gmail.com>
2022-10-31 22:50:15 +0500

    start project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyrogram import Client
 from data import api_id, api_hash
 
-print(api_id, api_hash)
 
+with Client("account", api_id, api_hash) as app:
+    app.send_message("79199496625", "Greetings from **Pyrogram** !")
